ccf63ea2c979886cef2f9e1ba63ff164639a43a2
DontStave <DontStave@email.com>
2024-04-17 18:19:22 +0800

    初始化项目

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,11 +1,14 @@
 <template>
   <div class="dashboard-container">
     <div class="dashboard-text">name: {{ name }}</div>
+    <div>就直接从这里开始吧！！！fighting</div>
+    <el-button type="primary" @click="firstClickFuction">我是一个按钮</el-button>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import axios from 'axios'
+import {mapGetters} from 'vuex'
 
 export default {
   name: 'Dashboard',
@@ -13,6 +16,19 @@ export default {
     ...mapGetters([
       'name'
     ])
+  },
+  data() {
+    return {
+      message: 'Hello Vue.js!'
+    }
+  },
+  methods: {
+    firstClickFuction: function() {
+    /*  this.$message('这是一条消息提示')*/
+      axios.get('http://localhost:8080/').then(response => {
+        this.$message(response.data)
+      })
+    }
   }
 }
 </script>
@@ -22,6 +38,7 @@ export default {
   &-container {
     margin: 30px;
   }
+
   &-text {
     font-size: 30px;
     line-height: 46px;
